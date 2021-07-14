94e812909572aa0352e04d45d712f52635b64d1c
mdafsha <mdafsha9@gmail.com>
2021-07-14 16:13:29 +0530

    invoking a directive using attribute

--- model.js
+++ model.js
@@ -12,6 +12,11 @@ app.controller("myCtrl", function($scope){
 
 app.directive("testDirective", function(){
   return{
-    template:"<h3>Custom DirectiveType-1:</h3><p>Invoking a directive using Element Name</p>"
+    template:"<h3>Custom DirectiveType-1:</h3><p>Invoking a directive by using Element Name</p>"
+  };
+});
+app.directive("test1Directive", function(){
+  return{
+    template:"<h3>Custom Directive Type-2</h3><p>Invoking a directive by using Attribute</p>"
   };
 });

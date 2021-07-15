23e2f036adab0ed803c302469273a2dadd0af7f9
mdafsha <mdafsha9@gmail.com>
2021-07-15 15:29:25 +0530

    about data binding

--- script.js
+++ script.js
@@ -2,8 +2,3 @@ var app = angular.module("myApp1", []);
 app.controller("myCtrl", function($scope){
   $scope.name = "John Doe";
 });
-app.controller("myCtrl", function($scope){
-  $scope.ctrlLists = ["Data binding in AngularJS is synchronization between the model and the view",
-  "AngularJS application usually have a data model. A data model is a collection of data available for the application",
-  "The HTML container where the AngularJS application is displayed is called a view"]
-})

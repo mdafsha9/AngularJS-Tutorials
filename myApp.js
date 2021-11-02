4013c289327df9973bc9f667bf842d2bec5b786f
mdafsha9 <mdafsha9@gmail.com>
2021-11-02 04:17:13 -0500

    invoke a directive using element name or attribute

--- myApp.js
+++ myApp.js
@@ -1,1 +1,4 @@
 var app = angular.module("myApp", []);
+
+//for directives
+var app1 = angular.module("myApp1", []);
--- myCtrl.js
+++ myCtrl.js
@@ -2,3 +2,11 @@ app.controller("myCtrl", function($scope){
   $scope.firstName = " ";
   $scope.lastName = " ";
 })
+
+//for Directives
+//invoke a directive using element name, attribute,class
+app1.directive("w3TestDirective", function(){
+  return{
+    template:"<h3>made a directive</h3>"
+  };
+});
